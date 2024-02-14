89c5ec5de311511934d29caf1e075a5bb254efed
StalinSamuelRaja <srstalinsr@gmail.com>
2024-02-14 17:18:53 +0530

    api.js

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,7 +1,7 @@
-// export const Ques_API="https://stackoverflow-xk2f.onrender.com/ques";
-// export const Answ_API="https://stackoverflow-xk2f.onrender.com/answ";
-// export const User_API="https://stackoverflow-xk2f.onrender.com/user";
-export const Ques_API="http://localhost:9000/ques";
-export const Answ_API="http://localhost:9000/answ";
-export const User_API="http://localhost:9000/user";
+export const Ques_API="https://stackoverflow-xk2f.onrender.com/ques";
+export const Answ_API="https://stackoverflow-xk2f.onrender.com/answ";
+export const User_API="https://stackoverflow-xk2f.onrender.com/user";
+// export const Ques_API="http://localhost:9000/ques";
+// export const Answ_API="http://localhost:9000/answ";
+// export const User_API="http://localhost:9000/user";
 
