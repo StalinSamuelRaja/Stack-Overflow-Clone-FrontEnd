cce1c7fce6bfa6194fa2098b702f25baaafb7829
StalinSamuelRaja <srstalinsr@gmail.com>
2024-03-12 13:28:14 +0530

    desc

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -16,7 +16,7 @@ export default function LandingPage() {
       <TopBar />
       <div className="mt-8 mb-0 mx-4 mb-44 md:mx-0 text-center md:text-left">
       <h1 className="text-4xl md:text-3xl font-bold text-white">
-      Begin your Journey Click  <span className="md:inline-block text-orange-600">START</span>  to ask Question in our Stack Overflow clone.</h1>
+      Begin your Journey   <span className="md:inline-block text-orange-600">Click START</span>  to ask Question in our Stack Overflow clone.</h1>
       </div>
       <div className="flex flex-col md:flex-row md:justify-center md:items-center md:space-x-8 mt-8">
         <div className="m-8 md:mr-0 md:ml-12">
